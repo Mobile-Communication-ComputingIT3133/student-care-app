72a6260070bb197fa6d94babca0d507c5dc9c4d9
Kasun Rajapaksha <139611912+Kasunchira@users.noreply.github.com>
2025-01-05 03:23:42 +0000

    home screen updated

--- careapp/screens/HomeScreen.js
+++ careapp/screens/HomeScreen.js
@@ -1,12 +1,63 @@
-import {View ,Text} from 'react-native';
-import React from 'react';
+import { View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import ProfileScreen from "./ProfileScreen";
+import CourseScreen from "./CourseScreen";
+import SubjectsScreen from "./SubjectsScreen";
 
-const HomeeScreen =()=>{
-    return(
-        <View>
-            <Text>Home Screen</Text>
-        </View>
-    );
+const Tab = createBottomTabNavigator();
+
+const HomeScreen = ({ route }) => {
+  const { student } = route.params;
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "Profile") {
+            iconName = "person";
+          } else if (route.name === "Course") {
+            iconName = "school";
+          } else if (route.name === "Subjects") {
+            iconName = "book-open";
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        headerShown: false,
+        tabBarActiveTintColor: "#70116d",
+        tabBarInactiveTintColor: "#888",
+        tabBarStyle: {
+          backgroundColor: "#fff",
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Profile"
+        initialParams={{ student: student }}
+        component={ProfileScreen}
+      />
+      <Tab.Screen
+        name="Course"
+        component={CourseScreen}
+        initialParams={{ student: student }}
+      />
+      <Tab.Screen
+        name="Subjects"
+        component={SubjectsScreen}
+        initialParams={{ student: student }}
+      />
+    </Tab.Navigator>
+  );
 };
 
-export default HomeeScreen;
+export default HomeScreen;
